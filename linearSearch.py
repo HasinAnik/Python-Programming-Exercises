bcec838585f19b17ee69db7e58ed6dea7d32bed6
HasinAnik <71354899+HasinAnik@users.noreply.github.com>
2021-02-16 07:45:35 -0500

    Update linearSearch.py

--- linearSearch.py
+++ linearSearch.py
@@ -1,16 +1,25 @@
 print("This is the homework for 2/14/2021 class")
 print("Linear Search: Start from the 1st element to the last element until it finds the value looking for ")
 
-def lSearch(num, list1):
-    for elem in range(len(list1)):
-        if (int(list1[elem]) == num):return elem
+def intSearch(num, list1):
+    list1 = users.split(",") #Here I am splitting all the commas to just get the elements.
+    list2 = [int(num) for num in list1]
+    for elem in range(len(list2)):
+        if (int(list2[elem]) == num):return elem
             #return "Invalid input"
     return -1
 
 num = int(input("Entr a number: "))
-users = input("Enter a list seperated by a space: ")
-list1 = users.split()
-for elem in range(len(list1)):
-    list1[elem] = int(list1[elem])
+users = input("Enter a integer list seperated by a comma: ")
+print("First Occurence of the num is " + str(intSearch(num,users)))
 
-print("First Occurence of the num is " + str(lSearch(num,list1)))
+
+def charSearch(char, list1):
+    list1 = users.split(",")
+    for elem in range(len(list1)):
+        if (list1[elem] == char):return elem
+    return -1
+
+char = input("Entr a char: ")
+users = input("Enter a char list seperated by a comma: ")
+print("First Occurence of the num is " + str(charSearch(char,users)))
